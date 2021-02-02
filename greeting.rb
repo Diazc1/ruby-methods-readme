b6f28bce3ac5be65eb655382b6aed41c02c67abc
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-02 15:33:31 -0800

    commit tree c3a866924aa78672bdee6ad245c9b2b2caf626fc

---
+++ greeting.rb
@@ -0,0 +1,9 @@
+def greeting
+    puts "Hello World"
+end
+#outcome is Hello World
+greeting
+greeting
+greeting
+greeting
+greeting    #Remember! That when you write it in your code, it should be greeting and not #greeting
